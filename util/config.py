091ec8fae8ccf412eb1f949da157cb30b33bd04d
DavidZhang <shihaoz@umich.edu>
2017-04-16 18:59:12 -0400

    feature: fetch f4m at startup

--- util/config.py
+++ util/config.py
@@ -3,4 +3,9 @@ import logging
 
 log_file = "proxy.log"
 log_level = logging.DEBUG
-logging.basicConfig(filename=log_file, level=log_level)
+log_mode = 'w'  # overwrite existing log file
+logging.basicConfig(
+	filename=log_file,
+	level=log_level,
+	filemode=log_mode
+)
--- util/request_filter.py
+++ util/request_filter.py
@@ -1,24 +1,54 @@
 """ module for transforming request/response """
 import logging
 import re
+import sys
+import urllib.request
 
 
-def readF4M(bitrate, request):
+def readF4M(bitrate, host, port):
 	""" read request from server """
 	logging.info("reading f4M")
-	if request.find(b"text/xml") != -1:
-		""" this is a f4m """
-		logging.info("this is a f4m")
-		logging.info(request.decode('utf-8'))
-		if len(bitrate) == 0:
-			""" 1st time see f4m, read the available bitrate """
-			rates = re.findall(b'bitrate="([0-9]+)"', request)
-			logging.info("bitrates available: {}".format(rates))
-			for rate in rates:
-				bitrate.append(int(rate.decode('utf-8')))
+	request = b""
+	try:
+		res = urllib.request.urlopen(
+			'http://{}:{}/vod/big_buck_bunny.f4m'
+			.format(host, port)
+		)
+		if res.getcode() != 200:
+			print(
+				'f4m request failed, code {}'.format(res.getcode()),
+				file=sys.stderr
+			)
+			logging.debug(
+				'f4m request failed, code {}'.format(res.getcode())
+			)
+			return
+		request = res.read()
+	except Exception as ex:
+		print('f4m request failed, exception: {}'.format(ex), file=sys.stderr)
+		logging.debug('f4m request failed, exception: {}'.format(ex))
+		return
+
+	""" this is a f4m """
+	logging.info("reading f4m")
+	logging.info(request.decode('utf-8'))
+	if len(bitrate) == 0:
+		rates = re.findall(b'bitrate="([0-9]+)"', request)
+		logging.info("bitrates available: {}".format(rates))
+		for rate in rates:
+			bitrate.append(int(rate.decode('utf-8')))
 			bitrate = sorted(bitrate)
-		request = request.replace(b'big_buck_bunny.f4m',
-		                          b'big_buck_bunny_nolist.f4m')
+	return
+
+
+def modifyF4M(request):
+	""" modify the f4m client request """
+	if request.find(b"/big_buck_bunny.f4m") != -1:
+		logging.info("this is a f4m")
+		request = request.replace(
+			b'big_buck_bunny.f4m',
+			b'big_buck_bunny_nolist.f4m'
+		)
 	return request
 
 
